cec03a697165489f941689392bb458da66397cd5
Gowtham  H D <gowthambhat793@gmail.com>
2021-05-23 18:33:02 +0530

    adding effects with js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeactiveClasses();
+    panel.classList.add('active');
+  });
+});
+
+function removeactiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+}
